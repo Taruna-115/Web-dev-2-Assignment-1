d60f3eef6f3208424fdb3cb499da291703a64dd2
Taruna Tewatia <tarunatewatia318@gmail.com>
2026-02-09 09:37:51 +0530

    Second Commit: added js file

---
+++ script.js
@@ -0,0 +1,12 @@
+// ================== DOM SELECTION ==================
+const titleInput = document.querySelector('input[type="text"]');
+const dateInput = document.querySelector('input[type="date"]');
+const categorySelect = document.querySelector('select');
+const descInput = document.querySelector('textarea');
+
+const addBtn = document.querySelector('.primary');
+const clearBtn = document.querySelectorAll('.secondary')[0];
+const sampleBtn = document.querySelectorAll('.secondary')[1];
+
+const eventsCard = document.querySelector('.card.large');
+const emptyText = document.querySelector('.empty-text');
